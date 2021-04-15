8fe52d4726baf8c9409d68eb4c41be9d8573656e
justin <flaminlinkvp@gmail.com>
2021-04-15 13:39:13 -0700

    phase-1 isOdd complete

--- problems/phase-1.js
+++ problems/phase-1.js
@@ -2,19 +2,25 @@
    to 5 and false if it is not.
 */
 function isFive(num) {
- if (num === 5) {
-   return 5;
- }
+  if (num === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /*2. Is Odd Write a function isOdd that takes in a number as an argument and
    returns true if the number is odd and returns false otherwise.
 */
 function isOdd(number) {
-  if (typeof number !== 'number'){
-    throw Error('some error')
+  if (typeof number !== 'number') {
+    throw new Error();
   }
-  return (number % 2 === 0);
+  if (number % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 /*3. Array  of the Range Write a function myRange(min, max, step) that takes in
@@ -22,23 +28,23 @@ function isOdd(number) {
       numbers between and inclusive of min and max at step intervals.
 */
 
-function myRange(min, max, step=1) {
+function myRange(min, max, step = 1) {
   const array = []
   for (let i = min; i <= max; i++) {
-    array.push(i);  
+    array.push(i);
   }
   return array
 }
 /*4. Fizz Buzz Define a function fizzBuzz(max) that takes a number and prints
    every number from 0 to max (not inclusive) that is divisible by either 3 or
-   5, but not both. 
+   5, but not both.
 */
 
 function fizzBuzz(max) {
-  if (typeof max !== 'number' ){
+  if (typeof max !== 'number') {
     throw TypeError('needs to be a number')
   }
-  if (max < 0){
+  if (max < 0) {
     throw new Error
   }
 
@@ -54,4 +60,4 @@ function fizzBuzz(max) {
 }
 
 
-module.exports = {isFive, isOdd, myRange, fizzBuzz }
+module.exports = { isFive, isOdd, myRange, fizzBuzz }
--- test/phase-1-spec.js
+++ test/phase-1-spec.js
@@ -1,48 +1,65 @@
 const chai = require("chai");
 const expect = chai.expect;
-const { isFive, isOdd, myRange, fizzBuzz} = require("../problems/phase-1");
-describe("isFive(num)", () => { 
+const { isFive, isOdd, myRange, fizzBuzz } = require("../problems/phase-1");
+describe("isFive(num)", () => {
   it("should return true if the num is 5 otherwise false", () => {
     //Arrange
+    let test = 5
+    let expectedValue = true;
+
     //Act
+    let testFunc = isFive(test)
+
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(testFunc).to.equal(expectedValue);
 
     //Arrange
-    
-    //Act
-    
-    //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    let test2 = 6
+    let expectedValue2 = false;
 
+    //Act
+    let testFunc2 = isFive(test2)
+
+    //Assert
+    expect(testFunc2).to.equal(expectedValue2)
   });
 });
 describe("isOdd(number)", () => {
   it("should return true if number is odd", () => {
     //Arrange
-   
+    let testNum = 1;
+    let expectedValue = true;
+
     //Act
+    let testFunc = isOdd(testNum);
 
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(testFunc).to.equal(expectedValue)
 
   });
   it("should return false if the num is even", () => {
     //Arrange
-    
+    let testNum = 2;
+    let expectedValue = false;
+
     //Act
-    
+    let testFunc = isOdd(testNum)
+
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(testFunc).to.equal(expectedValue)
 
   });
   it("should throw an error if num is not type of Number", () => {
     //Arrange
-    
+    let testNum = 'number'
+
     //Act
-    
+    let testFunc = () => {
+      isOdd(testNum)
+    }
+
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(testFunc).to.throw(Error)
 
   });
 });
@@ -50,9 +67,9 @@ describe("myRange(min, max, step)", () => {
   context("if step is not provided", () => {
     it("should return the correct array with default value step=1", () => {
       //Arrange
-      
+
       //Act
-      
+
       // Assert
       expect.fail('Remove this expect.fail and replace it with your test');
 
@@ -61,9 +78,9 @@ describe("myRange(min, max, step)", () => {
   context("if step is provided", () => {
     it("should return the correct array", () => {
       //Arrange
-     
+
       //Act
-     
+
       // Assert
       expect.fail('Remove this expect.fail and replace it with your test');
 
@@ -71,38 +88,38 @@ describe("myRange(min, max, step)", () => {
   });
   it("should throw an error if num is not type of Number", () => {
     //Arrange
-   
+
     //Act
-   
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
   });
 });
-describe('fizzBuzz(max)', ()=> {
-  it('should throw a RangeError if max < 0', ()=> {
+describe('fizzBuzz(max)', () => {
+  it('should throw a RangeError if max < 0', () => {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
   })
-  it('should throw a TypeError if max is not a number', ()=> {
+  it('should throw a TypeError if max is not a number', () => {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
   })
-  it('should return an array from 0 to max (not inclusive) of numbers that are divisible by either 3 or 5 but not both', ()=> {
+  it('should return an array from 0 to max (not inclusive) of numbers that are divisible by either 3 or 5 but not both', () => {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
